244d512cd75c76445eea6fd4bf89c96f88a3c55d
Isaac Ojo <hojoisaac@gmail.com>
2023-07-24 09:36:28 +0100

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
-import { Inter, Plus_Jakarta_Sans } from "next/font/google";
-import { Box, Button } from "@chakra-ui/react";
-
-
+"use client";
+import { Box, Center, Flex, Spinner, Text } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("shop");
+  }, [router]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <Center height={"100vh"} width={"100vw"}>
+      <Flex alignItems={'center'} justifyContent={'center'} flexDirection={'column'}>
+        <Spinner />
+        <Text textAlign={'center'}>Redirecting to shop</Text>
+      </Flex>
+    </Center>
   );
 }
